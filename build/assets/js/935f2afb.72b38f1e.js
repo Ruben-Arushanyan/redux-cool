d9223f493e3a618ead3146cef27d1b7c0a7e37b2
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2021-04-20 22:56:17 +0400

    delete build

--- build/assets/js/935f2afb.72b38f1e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{75:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{"tutorialSidebar":[{"type":"link","label":"Redux Cool","href":"/redux-cool/docs/"}]},"permalinkToSidebar":{"/redux-cool/docs/":"tutorialSidebar"}}')}}]);
