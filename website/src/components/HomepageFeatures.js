28038515d69cee3dd25a3dc4bf3b01682a4ebc34
Ruben Arushanyan <ruben.arushanyan24@gmail.com>
2021-04-27 00:19:22 +0400

    docs improvements

--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2020-present Ruben Arushanyan (https://github.com/ruben-arushanyan)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ */
+
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2020-present Ruben Arushanyan (https://github.com/ruben-arushanyan)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ * 
+ */
+
 import React from 'react';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
